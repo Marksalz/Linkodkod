a33a9bec3dae5781adf431d3d35a5c61f3d5bdd8
Mark <menachemsalzberg13@gmail.com>
2025-09-07 14:49:44 +0300

    Add homepage with basic css. Load data foem json file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,16 @@
 import "./App.css";
-import Post from "./components/Post";
-import type PostInfo from "./interfaces/postDataType";
+import mockPosts from "./assets/mockPosts.json";
+import Homepage from "./pages/Homepage";
+
+const posts = mockPosts;
+console.log(posts);
 
 function App() {
   return (
     <>
-      <Post postInfo={posts} />
+      <Homepage posts={posts} />
     </>
   );
 }
 
 export default App;
-
-const posts: PostInfo = {
-  imgSrc:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDYcBgDodY-2g5vAtnwSBW2l4PNWTkL0HHzw&s",
-  likes: 344,
-  description: "cute cat pic hasajkahs asanskajskajsa saskajsajsa sanskasj sdkjdsjd dsnkdsnkdsc cnksncksc sncksncks ",
-  name: "Menachem Salzberg",
-  timestamp: "09/07/2025 12:00pm",
-};
